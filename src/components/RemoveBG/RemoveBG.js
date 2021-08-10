d723ea63fa47e518ffe96463c7c9bfaf65613162
parthdhake <parthdhake@gmail.com>
2021-08-10 23:58:12 +0530

    Warnings Removed

--- src/components/RemoveBG/RemoveBG.js
+++ src/components/RemoveBG/RemoveBG.js
@@ -15,35 +15,6 @@ export default function RemoveBG() {
 
     const [image, setImage] = useState(null);
 
-    const downloadBlob = (blob, name = 'file.png') => {
-        if (
-        window.navigator && 
-        window.navigator.msSaveOrOpenBlob
-        ) return window.navigator.msSaveOrOpenBlob(blob);
-
-        // For other browsers:
-        // Create a link pointing to the ObjectURL containing the blob.
-        const data = blob;
-
-        const link = document.createElement('a');
-        link.href = data;
-        link.download = name;
-
-        // this is necessary as link.click() does not work on the latest firefox
-        link.dispatchEvent(
-            new MouseEvent('click', { 
-                bubbles: true, 
-                cancelable: true, 
-                view: window 
-            })
-        );
-
-        setTimeout(() => {
-        // For Firefox it is necessary to delay revoking the ObjectURL
-        window.URL.revokeObjectURL(data);
-        link.remove();
-        }, 100);
-    }
     const imgUpload = (e) => {
         const i = e.target.files[0];
         setImage(i);
@@ -99,7 +70,7 @@ export default function RemoveBG() {
             </div>
             <div>
                 {status ? <div className="image">
-                    <img id="result" src={b} />
+                    <img id="result" alt="" src={b} />
                     {/* <a href={b} download>Click to download</a>
 
                     <button onClick={downloadBlob.bind(this, b)}></button> */}
