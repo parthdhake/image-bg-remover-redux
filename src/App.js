ef32c78e665e61923b8857fa92308e456d86c423
parthdhake <parthdhake@gmail.com>
2021-08-11 00:09:33 +0530

    CSS

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Bacground Removal Tool</p>
+        <p>Image Background Removal Tool</p>
       </header>
       <RemoveBG />
 
